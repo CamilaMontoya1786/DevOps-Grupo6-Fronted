8948dc9997fe5961931ac57cfdce6a44a3c47d41
paolaruiz <paola02995@gmail.com>
2024-09-21 08:52:53 -0500

    In this functionality the register, validations, post method, everything was created.

--- Mis_luquitas/client/src/App.jsx
+++ Mis_luquitas/client/src/App.jsx
@@ -1,31 +1,20 @@
-import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import Register from './page/register'
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./page/register";
+import { AuthProvider } from "./context/authContext";
 
 function App() {
-
-  return(
-    
+  return (
     <>
-    
-      <BrowserRouter>
-
-        <Routes>
-
-          <Route path='/register' element={<Register></Register>}></Route>
-
-         </Routes>
-
-      </BrowserRouter> 
-
-   
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/register" element={<Register></Register>}></Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </>
-
-
-
-  )
-  
+  );
 }
 
-export default App
+export default App;
