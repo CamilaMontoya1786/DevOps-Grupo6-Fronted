5fc3f53cbe9abd926f9aaf7f4378e5e9c78983ae
PaolaRuizOsorio <126583659+PaolaRuizOsorio@users.noreply.github.com>
2024-09-27 21:42:45 -0500

    Delete Mis_luquitas directory

--- Mis_luquitas/client/src/api/auth.js
+++
@@ -1,5 +0,0 @@
-import axios  from "./axios";
-/*conexion backend - frontend*/
-
-export const registerRequest = user => axios.post('/loguin/create',user)
-export const loguinRequest = user => axios.post('/loguin/loguin',user)
