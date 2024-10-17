0521be66abd5c511d86e6013bdaea9342778d01b
paolaruiz <paola02995@gmail.com>
2024-10-17 18:31:23 -0500

    En esta actualizacion se realizaron los tres componente sy vistas de movimientos, adicional la navegacion entre pesatañas y la verificacion en el ologuin

--- Mis_luquitas/client/src/App.jsx
+++ Mis_luquitas/client/src/App.jsx
@@ -2,19 +2,23 @@ import "./App.css"; // Esto se puede quitar si no hay estilos globales
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Register from "./page/register";
 import { AuthProvider } from "./context/authContext";
-import Login from "./page/loguin";
+import Login from "./page/login";
 import Restorepassword from "./page/restorepassword";
 import Navbar from "./components/Navbar";
 import Home from "./page/Home";
 import UserProfile from "./page/userprofile";
 import Changepassword from "./page/changePassword";
 import Help from "./page/help";
+import Movements from "./page/movements";
+import Expenses from "./page/Expenses";
+import Income from "./page/Income";
 
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+      
         <MainRoutes /> {/* Movemos la lógica de las rutas a un componente separado */}
       </BrowserRouter>
     </AuthProvider>
@@ -23,19 +27,24 @@ function App() {
 
 function MainRoutes() {
   const location = useLocation();
-  const hideNavbar = ["/register", "/login", "/restorepassword", "/userprofile", "/changePassword"].includes(location.pathname);
+  const hideNavbar = ["/register", "/login", "/restorepassword", "/userprofile", "/changePassword", "/expenses", "/income"].includes(location.pathname);
 
   return (
     <>
       {!hideNavbar && <Navbar />}
       <Routes>
+        
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} /> {/* Corregido "loguin" */}
+        <Route path="/login" element={<Login />} /> 
         <Route path="/restorepassword" element={<Restorepassword />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/userprofile" element={<UserProfile />} />
         <Route path="/changePassword" element={<Changepassword />} />
         <Route path="/help" element={<Help/>} />
+        <Route path="/movements" element={<Movements />} />
+        <Route path="/expenses" element={<Expenses />} />
+        <Route path="/income" element={<Income />} />
+        
       </Routes>
     </>
   );
--- Mis_luquitas/client/src/page/register.jsx
+++ Mis_luquitas/client/src/page/register.jsx
@@ -1,73 +1,57 @@
-import React, { useState, useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { useAuth } from "../context/authContext";
-import LogoImage from "../imagine/logo.png";
-import styles from "../styles/register.module.css";
-import axios from "axios";
+import React, { useState, useEffect } from "react"; 
+import { useForm } from "react-hook-form"; 
+import { useAuth } from "../context/authContext"; 
+import LogoImage from "../imagine/logo.png"; 
+import styles from "../styles/register.module.css"; 
+import axios from "axios"; 
 
 function Register() {
-  const [data, setData] = useState([]);
-  const [userList, setUserList] = useState([]);
-  const [showHelperMessage, setShowHelperMessage] = useState(false);
-  // Efecto para gestionar la clase del body para estilos
-  useEffect(() => {
-    document.body.classList.add("special-body-register"); // Agrega clase especial al montarse
-
-    // Función de limpieza para eliminar la clase al desmontarse
-    return () => {
-      document.body.classList.remove("special-body-register");
-    };
-  }, []);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ mode: "onBlur" });
-  const { signup } = useAuth();
-  const [errorMessage, setErrorMessage] = useState("");
+  const [data, setData] = useState([]); 
+  const [userList, setUserList] = useState([]); 
+  const [showHelperMessage, setShowHelperMessage] = useState(false); 
+  const { register, handleSubmit, formState: { errors } } = useForm({ mode: "onBlur" }); 
+  const { signup } = useAuth(); 
+  const [errorMessage, setErrorMessage] = useState(""); 
 
   useEffect(() => {
     axios
       .get("http://localhost:3000/identification/identifications")
       .then((response) => {
-        setData(response.data);
+        setData(response.data); 
       })
       .catch((error) => {
-        console.error("Error al obtener los datos:", error);
+        console.error("Error al obtener los datos:", error); 
       });
+      console.log(setData)
   }, []);
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const response = await axios.get(
-          "http://localhost:3000/country/countries"
-        );
-        setUserList(response.data);
+        const response = await axios.get("http://localhost:3000/country/countries");
+        setUserList(response.data); 
       } catch (error) {
-        console.error("Error al obtener los datos:", error);
+        console.error("Error al obtener los datos:", error); 
       }
     };
-
-    fetchUserData();
+    fetchUserData(); 
   }, []);
 
   const onSubmit = async (value) => {
-    console.log("Submitting:", value); // Verificar que se envían los datos
+    console.log("Submitting:", value); 
     try {
-      await signup(value);
-      console.log("Registro exitoso");
+      await signup(value); 
+      console.log("Registro exitoso"); 
     } catch (error) {
-      console.error("Error al registrarse:", error);
-      setErrorMessage("Error al registrarse. Por favor, intenta de nuevo.");
+      console.error("Error al registrarse:", error); 
+      setErrorMessage("Error al registrarse. Por favor, intenta de nuevo."); 
     }
   };
 
   const handleBeforeInput = (event) => {
-    const char = event.data;
+    const char = event.data; 
     if (!/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]$/.test(char)) {
-      event.preventDefault();
+      event.preventDefault(); 
     }
   };
 
@@ -81,18 +65,19 @@ function Register() {
 
         <div className={styles.body_register}>
           <h2>Registrarse</h2>
-          <hr></hr>
+          <hr />
+
+          {errorMessage && <span className={styles.error_message}>{errorMessage}</span>} {/* Muestra mensaje de error global */}
 
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className={styles.inputPadre}>
-              {/* Nombre ------------------------------------------------------------------------------------------ */}
               <div className={styles.inputContainer}>
                 <input
                   name="userName"
                   type="text"
                   placeholder="Nombre"
                   {...register("userName", {
-                    required: true,
+                    required: "Este campo es obligatorio",
                     pattern: {
                       value: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/,
                       message: "El nombre solo puede contener letras",
@@ -100,19 +85,15 @@ function Register() {
                   })}
                   onBeforeInput={handleBeforeInput}
                 />
-
                 {errors.userName && (
-                  <span className={styles.error_message}>
-                    {errors.userName.message}
-                  </span>
+                  <span className={styles.error_message}>{errors.userName.message}</span>
                 )}
 
-                {/* Apellido */}
                 <input
                   type="text"
                   placeholder="Apellido"
                   {...register("userLastName", {
-                    required: true,
+                    required: "Este campo es obligatorio",
                     pattern: {
                       value: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/,
                       message: "Solo se permiten letras",
@@ -120,90 +101,73 @@ function Register() {
                   })}
                   onBeforeInput={handleBeforeInput}
                 />
-
                 {errors.userLastName && (
-                  <span className={styles.error_message1}>
-                    {errors.userLastName.message}
-                  </span>
+                  <span className={styles.error_message}>{errors.userLastName.message}</span>
                 )}
-
-                {/* Campo de correo electrónico */}
 
                 <input
                   type="email"
                   placeholder="Correo electrónico"
                   {...register("email", {
-                    required: true,
+                    required: "Este campo es obligatorio",
                     pattern: {
                       value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
                       message: "Formato de correo electrónico inválido",
                     },
                   })}
                 />
-
                 {errors.email && (
-                  <span className={styles.error_message}>
-                    {errors.email.message}
-                  </span>
+                  <span className={styles.error_message}>{errors.email.message}</span>
                 )}
-                {/* Campo de contraseña */}
 
                 <input
                   type="password"
                   placeholder="Contraseña"
-                  onFocus={() => setShowHelperMessage(true)} // Muestra el mensaje al hacer clic
-                  onBlur={() => setShowHelperMessage(false)} // Oculta el mensaje al salir del campo
+                  onFocus={() => setShowHelperMessage(true)}
+                  onBlur={() => setShowHelperMessage(false)}
                   {...register("password", {
-                    required: true,
+                    required: "Este campo es obligatorio",
                     minLength: {
                       value: 8,
                       message: "La contraseña debe tener al menos 8 caracteres",
                     },
                     pattern: {
                       value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/,
+                      message: "La contraseña debe incluir al menos un número, una letra mayúscula, una letra minúscula y un carácter especial",
                     },
                   })}
                 />
                 {showHelperMessage && (
                   <span className={styles.helper_message}>
-                    La contraseña debe incluir al menos un número, una letra
-                    mayúscula, una letra minúscula y un carácter especial
+                    La contraseña debe incluir al menos un número, una letra mayúscula, una letra minúscula y un carácter especial
                   </span>
                 )}
                 {errors.password && (
-                  <span className={styles.error_message}>
-                    {errors.password.message}
-                  </span>
+                  <span className={styles.error_message}>{errors.password.message}</span>
                 )}
               </div>
 
-              {/* Campo de tipo de documento  */}
               <div className={styles.input_container}>
-                <select {...register("typeId", { required: true })}>
+                <select {...register("typeId", { required: "Este campo es obligatorio" })}>
                   <option value="">Seleccione un tipo de documento...</option>
                   {data.map((identity) => (
                     <option key={identity.typeId} value={identity.typeId}>
-                      {identity.idDescription}
+                      {identity.idDescription} 
                     </option>
                   ))}
                 </select>
                 {errors.typeId && (
-                  <span className={styles.error_message}>
-                    {errors.typeId.message}
-                  </span>
+                  <span className={styles.error_message}>{errors.typeId.message}</span>
                 )}
-
-                {/* Campo de número de documento */}
 
                 <input
                   type="text"
                   placeholder="Número de documento"
                   {...register("idNumber", {
-                    required: true,
+                    required: "Este campo es obligatorio",
                     pattern: {
                       value: /^[0-9]{6,10}$/,
-                      message:
-                        "El número de documento debe tener entre 6 y 10 dígitos",
+                      message: "El número de documento debe tener entre 6 y 10 dígitos",
                     },
                   })}
                   onBeforeInput={(event) => {
@@ -213,39 +177,30 @@ function Register() {
                     }
                   }}
                 />
-
                 {errors.idNumber && (
-                  <span className={styles.error_message1}>
-                    {errors.idNumber.message}
-                  </span>
+                  <span className={styles.error_message}>{errors.idNumber.message}</span>
                 )}
 
-                {/* Campo de país */}
-
-                <select {...register("countryId", { required: true })}>
+                <select {...register("countryId", { required: "Este campo es obligatorio" })}>
                   <option value="">Seleccione un país...</option>
                   {userList.map((country) => (
                     <option key={country.countryId} value={country.countryId}>
-                      {`${country.countryId}. ${country.countryName}`}{" "}
-                      {/* Muestra el ID y el nombre del país aquí */}
+                      {country.countryName}
                     </option>
                   ))}
                 </select>
                 {errors.countryId && (
-                  <span className={styles.error - message1}>
-                    {errors.countryId.message}
-                  </span>
+                  <span className={styles.error_message}>{errors.countryId.message}</span>
                 )}
-                {/* Campo de número de telefono */}
+
                 <input
                   type="text"
                   placeholder="Número de teléfono"
                   {...register("phone", {
-                    required: true,
+                    required: "Este campo es obligatorio",
                     pattern: {
                       value: /^[0-9]{7,15}$/,
-                      message:
-                        "El número de teléfono debe tener entre 7 y 15 dígitos",
+                      message: "El número de teléfono debe tener entre 7 y 15 dígitos",
                     },
                   })}
                   onBeforeInput={(event) => {
@@ -256,13 +211,11 @@ function Register() {
                   }}
                 />
                 {errors.phone && (
-                  <span className={styles.error_message1}>
-                    {errors.phone.message}
-                  </span>
+                  <span className={styles.error_message}>{errors.phone.message}</span>
                 )}
               </div>
             </div>
-            <hr></hr>
+            <hr />
             <div className={styles.button_container}>
               <button type="submit" className={styles.button_register}>
                 Registrar
@@ -277,4 +230,4 @@ function Register() {
   );
 }
 
-export default Register;
+export default Register; 
